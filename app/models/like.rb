2f2c4290750b5d1afb0a0804ea77d759daad7aa3
unknown <sharonkessio6@gmail.com>
2023-01-27 08:03:41 +0300

    create relations between like and post models

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,3 +1,4 @@
 # rubocop:disable all
 class Like < ApplicationRecord
+    belongs_to :post, class_name: 'Post', foreign_key: :post_id
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 # rubocop:disable all
 class Post < ApplicationRecord
-    has_many :comments, foreign_key: :post_id
+    has_many :comments, class_name: 'Comment', foreign_key: :post_id
+    has_many :likes, class_name: 'Like', foreign_key: :post_id
 
     belongs_to :author, class_name: 'User', foreign_key: :author_id 
 end
