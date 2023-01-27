30a5712b980132bb3bd98195fe3c7bc3a5c3d849
unknown <sharonkessio6@gmail.com>
2023-01-27 09:19:06 +0300

    Create post methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,12 @@ class Post < ApplicationRecord
     has_many :likes, class_name: 'Like', foreign_key: :post_id
 
     belongs_to :author, class_name: 'User', foreign_key: :author_id 
+
+    def update_post_count
+        author.increment!(:posts_counter)
+    end
+
+    def recent_comments
+        Comment.order(created_at: :desc).limit(5)
+    end
 end
