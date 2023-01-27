98472d7cb4c66c120b154ef1ba384397978b9307
unknown <sharonkessio6@gmail.com>
2023-01-27 07:56:20 +0300

    create model relations between user and post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 # rubocop:disable all
 class Post < ApplicationRecord
     has_many :comments, foreign_key: :post_id
+
+    belongs_to :author, class_name: 'User', foreign_key: :author_id 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 # rubocop:disable all
 class User < ApplicationRecord
+    has_many :posts, class_name: 'Post', foreign_key: :author_id
 end
