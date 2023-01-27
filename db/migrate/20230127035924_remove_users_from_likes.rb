6cacb51ecc992cd78df499f32fc546eba9dd37ab
unknown <sharonkessio6@gmail.com>
2023-01-27 07:01:01 +0300

    Update user references to likes

---
+++ db/migrate/20230127035924_remove_users_from_likes.rb
@@ -0,0 +1,5 @@
+class RemoveUsersFromLikes < ActiveRecord::Migration[7.0]
+  def change
+    remove_reference :likes, :user, null: false, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_27_035158) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_27_035924) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,12 +27,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_27_035158) do
   create_table "likes", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id", null: false
     t.bigint "post_id"
     t.bigint "author_id"
     t.index ["author_id"], name: "index_likes_on_author_id"
     t.index ["post_id"], name: "index_likes_on_post_id"
-    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -58,7 +56,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_27_035158) do
   add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users", column: "author_id"
   add_foreign_key "likes", "posts"
-  add_foreign_key "likes", "users"
   add_foreign_key "likes", "users", column: "author_id"
   add_foreign_key "posts", "users", column: "author_id"
 end
