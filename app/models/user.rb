4a9d5aa5fe7567b3ec6e0268df952d28ab013af2
unknown <sharonkessio6@gmail.com>
2023-01-27 08:58:03 +0300

    Create user methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,9 @@
 class User < ApplicationRecord
     has_many :posts, class_name: 'Post', foreign_key: :author_id
     has_many :comments, class_name: 'Comment', foreign_key: :author_id
-    has_many :likes, class_name: 'Like, foreign_key: :author_id
+    has_many :likes, class_name: 'Like', foreign_key: :author_id
+
+    def recent_posts
+        Post.order(created_at: :desc).includes(:author).limit(3)
+    end
 end
