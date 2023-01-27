e7eba0bb76cbfad827fb703738121de306891bd0
unknown <sharonkessio6@gmail.com>
2023-01-27 13:23:26 +0300

    validate user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   has_many :comments, class_name: 'Comment', foreign_key: :author_id
   has_many :likes, class_name: 'Like', foreign_key: :author_id
 
+  validate :name, presence: true
+  validate :posts_counter, numericality: { only integer: true, greater_than_or_equal_to: 0 }
+
   def recent_posts
     Post.order(created_at: :desc).includes(:author).limit(3)
   end
