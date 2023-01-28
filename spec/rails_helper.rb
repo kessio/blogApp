45ee74037e22c959e31b2cc9990b8afe01d3bebb
unknown <sharonkessio6@gmail.com>
2023-01-28 08:00:16 +0300

    Enable rubocop in rails_helper and fix bug

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,4 +1,3 @@
-# rubocop:disable all
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
@@ -63,4 +62,3 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 end
-# rubocop:enable all
