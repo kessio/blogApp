b5dd9166d4120fd38cb18182d2917c775e21bc6a
unknown <sharonkessio6@gmail.com>
2023-02-04 19:26:30 +0300

    fix rubocop erro

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,7 +2,7 @@ class LikesController < ApplicationController
   def create
     post = Post.find(params[:post_id])
 
-    Like.create(author: current_user, post: post)
+    Like.create(author: current_user, post:)
 
     redirect_to user_post_path(post.author, post)
   end
