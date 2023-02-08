13e2fd2dd387211751dbbfdbdab7419f22960757
unknown <sharonkessio6@gmail.com>
2023-02-08 09:27:18 +0300

    fix recent posts

--- spec/views/users/users_show_spec.rb
+++ spec/views/users/users_show_spec.rb
@@ -2,25 +2,32 @@ require 'rails_helper'
 
 RSpec.describe 'User Page Show', type: :feature do
     before :each do
-        @tom = User.create(
+        @user = User.create(
             name: 'Tom',
             photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
             bio: 'I love cats',
-            posts_counter: 2
+            posts_counter: 3,
+            id: 2
         )
         @post1 = Post.create(
             text: 'This is my first post',
             comments_counter: 2,
             likes_counter: 3,
-            author_id: @tom.id
+            author_id: @user.id
         )
         @post2 = Post.create(
-            text: 'This is my first post',
+            text: 'This is my second post',
             comments_counter: 2,
             likes_counter: 3,
-            author_id: @tom.id
+            author_id: @user.id
         )
-        visit user_path(@tom.id)
+        @post3 = Post.create(
+            text: 'This is my third post',
+            comments_counter: 2,
+            likes_counter: 3,
+            author_id: @user.id
+        )
+        visit user_path(@user.id)
     end
 
     it 'shows the profile picture of the user' do
@@ -29,15 +36,19 @@ RSpec.describe 'User Page Show', type: :feature do
       end
 
       it 'shows the username' do
-        expect(page).to have_content(@tom.name)
+        expect(page).to have_content(@user.name)
       end
 
       it 'shows the bio' do
-        expect(page).to have_content(@tom.bio)
+        expect(page).to have_content(@user.bio)
         end
 
       it 'shows  number of posts' do
-        expect(page).to have_content(@tom.posts_counter)
+        expect(page).to have_content(@user.posts_counter)
+      end
+
+      it 'shows user first three posts' do
+        expect(page).to have_content(@post1.text)
       end
 
 
